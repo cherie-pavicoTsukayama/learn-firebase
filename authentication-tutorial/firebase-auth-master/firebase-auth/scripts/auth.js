1e127073aa0d4dd48a5c705acc526df6ad2ad487
Cherie <cherietsukayama@gmail.com>
2020-08-28 15:46:45 +0000

    sign the current user out.

--- authentication-tutorial/firebase-auth-master/firebase-auth/scripts/auth.js
+++ authentication-tutorial/firebase-auth-master/firebase-auth/scripts/auth.js
@@ -10,10 +10,19 @@ signupForm.addEventListener("submit", (e) => {
   //sign up the user
   auth.createUserWithEmailAndPassword(email, password)
     .then(cred => {
-      console.log(cred.user);
       const modal =  document.querySelector('#modal-signup');
       M.Modal.getInstance(modal).close();
       signupForm.reset();
     })
 
+})
+
+//LOGOUT USER
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut()
+    .then(() => {
+      console.log("user signed out");
+    })
 })
